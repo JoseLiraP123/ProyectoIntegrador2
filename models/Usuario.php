4d0dc51e87790feec968cd19ea20f55c558bd1c5
Javier <sanchez.jsebastian99@gmail.com>
2023-10-07 22:22:46 -0500

    Vista mantenimiento usuario

--- models/Usuario.php
+++ models/Usuario.php
@@ -1,50 +1,98 @@
 <?php
-    
-    class Usuario extends Conectar{
-        
-        public function login(){
-            $conectar= parent::Conexion();
-            parent::set_names();
-            
-            if(isset($_POST["enviar"])){
-                $correo = $_POST["usu_correo"];
-                $pass = $_POST["usu_pass"];
-                $rol = $_POST["rol_id"];
-                
-                if(empty($correo) and empty($pass)){
-                
-                    header("Location:".conectar::ruta()."index.php?m=2");
+
+class Usuario extends Conectar
+{
+
+    public function login()
+    {
+        $conectar = parent::Conexion();
+        parent::set_names();
+
+        if (isset($_POST["enviar"])) {
+            $correo = $_POST["usu_correo"];
+            $pass = $_POST["usu_pass"];
+            $rol = $_POST["rol_id"];
+
+            if (empty($correo) and empty($pass)) {
+
+                header("Location:" . conectar::ruta() . "index.php?m=2");
+                exit();
+            } else {
+                $sql = "SELECT * FROM tm_usuario WHERE usu_correo = ? AND usu_pass =? AND rol_id=? AND est = 1;";
+
+                $stmr = $conectar->prepare($sql);
+                $stmr->bindValue(1, $correo);
+                $stmr->bindValue(2, $pass);
+                $stmr->bindValue(3, $rol);
+                $stmr->execute();
+
+                $resultado = $stmr->fetch();
+
+                if (is_array($resultado) and count($resultado) > 0) {
+                    $_SESSION["usu_id"] = $resultado["usu_id"];
+                    $_SESSION["usu_nom"] = $resultado["usu_nom"];
+                    $_SESSION["usu_ape"] = $resultado["usu_ape"];
+                    $_SESSION["rol_id"] = $resultado["rol_id"];
+                    header("Location:" . conectar::ruta() . "view/Home/");
                     exit();
                 } else {
-                    $sql = "SELECT * FROM tm_usuario WHERE usu_correo = ? AND usu_pass =? AND rol_id=? AND est = 1;";
-                    
-                    $stmr = $conectar->prepare($sql);
-                    $stmr->bindValue(1, $correo);
-                    $stmr->bindValue(2, $pass);
-                    $stmr->bindValue(3, $rol);
-                    $stmr->execute();
-                    
-                    $resultado = $stmr->fetch();
-                    
-                    if(is_array($resultado) and count($resultado)>0){
-                        $_SESSION["usu_id"] = $resultado["usu_id"];
-                        $_SESSION["usu_nom"] = $resultado["usu_nom"];
-                        $_SESSION["usu_ape"] = $resultado["usu_ape"];
-                        $_SESSION["rol_id"] = $resultado["rol_id"];
-                        header("Location:".conectar::ruta()."view/Home/");
-                        exit();
-                    } else {
-                        header("Location:".conectar::ruta()."index.php?m=1");
-                        exit();
-                    }
-                    
+                    header("Location:" . conectar::ruta() . "index.php?m=1");
+                    exit();
                 }
-                
             }
-            
         }
-        
     }
-    
-?>
 
+    public function insert_usuario($usu_nom, $usu_ape, $usu_correo, $usu_pass, $rol_id)
+    {
+        $conectar = parent::Conexion();
+        parent::set_names();
+        $sql = "INSERT INTO tm_usuario (usu_id, usu_nom, usu_ape, usu_correo, usu_pass, rol_id, fech_crea, fech_modi, fech_elim,est) VALUES(NULL,?,?,?,?,?,now(),NULL,NULL,'1')";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $usu_nom);
+        $sql->bindValue(2, $usu_ape);
+        $sql->bindValue(3, $usu_correo);
+        $sql->bindValue(4, $usu_pass);
+        $sql->bindValue(5, $rol_id);
+        $sql->execute();
+        return $resultado = $sql->fetchAll();
+    }
+
+    public function update_usuario()
+    {
+    }
+
+    public function delete_usuario($usu_id)
+    {
+        $conectar = parent::Conexion();
+        parent::set_names();
+        $sql = "UPDATE tm_usuario SET est='0' WHERE usu_id=?";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $usu_id);
+
+        $sql->execute();
+        return $resultado = $sql->fetchAll();
+    }
+
+    public function get_usuario()
+    {
+        $conectar = parent::Conexion();
+        parent::set_names();
+        $sql = "SELECT * FROM tm_usuario WHERE est='0'";
+        $sql = $conectar->prepare($sql);
+
+        $sql->execute();
+        return $resultado = $sql->fetchAll();
+    }
+
+    public function get_usuario_x_id($usu_id)
+    {
+        $conectar = parent::Conexion();
+        parent::set_names();
+        $sql = "SELECT * FROM tm_usuario WHERE usu_id=?";
+        $sql = $conectar->prepare($sql);
+        $sql->bindValue(1, $usu_id);
+        $sql->execute();
+        return $resultado = $sql->fetchAll();
+    }
+}
---
+++ view/MntUsuario/index.php
@@ -0,0 +1,64 @@
+<?php
+    require_once '../../config/conexion.php';
+    if(isset($_SESSION["usu_id"])){ 
+?>
+
+<!DOCTYPE html>
+<html>
+    <?php require_once("../MainHeader/head.php"); ?>
+    
+<title>Mantenimiento usuario</title>
+</head>
+<body class="with-side-menu">
+
+	<?php require_once("../MainHeader/header.php"); ?>
+
+	<div class="mobile-menu-left-overlay"></div>
+        
+	<?php require_once("../MainNav/nav.php"); ?>
+
+	<div class="page-content">
+		<div class="container-fluid">
+                    <header class="section-header">
+                        <div class="tbl">
+                            <div class="tbl-row">
+                                <div class="tbl-cell">
+                                    <h3>Mantenimiento usuario</h3>
+                                    <ol class="breadcrumb breadcrumb-simple">
+                                            <li><a href="#">Home</a></li>
+                                            <li class="active">Mantenimiento usuario</li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </header>
+                    
+                    <div class="box-typical box-typical-padding">
+                        <table id="ticket_data" class="table table-bordered table-stripped table-vcenter js-dataTable-full">
+                            <thead>
+                                <tr>
+                                    <th style="width: 5%;">Nro. Ticket</th>
+                                    <th style="width: 15%;">Categoría</th>
+                                    <th class="d-none d-sm-table-cell" style="width: 40%">Título</th>
+                                    <th class="d-none d-sm-table-cell" style="width: 5%">Estado</th>
+                                    <th class="d-none d-sm-table-cell" style="width: 10%">Fecha de creación</th>
+                                    <th class="text-center" style="width: 5%;"></th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+		</div><!--.container-fluid-->
+	</div><!--.page-content-->
+
+	<?php require_once("../MainJS/js.php"); ?>
+        <script src="consultarticket.js"></script>
+        
+</body>
+</html>
+<?php  
+    } else {
+        $conexion = new Conectar();
+        $ruta = $conexion->ruta();        
+        header("Location: ".$ruta."index.php");
+    }
+?>
