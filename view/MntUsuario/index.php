5193bbf1c96bd375115d4b60e3ad99127d727f27
Javier <sanchez.jsebastian99@gmail.com>
2023-10-08 22:49:50 -0500

    Mantenimiento usuario correcion

--- view/MntUsuario/index.php
+++ view/MntUsuario/index.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
     <?php require_once("../MainHead/head.php");?>
-	<title>AnderCode</>::Mantenimiento Usuario</title>
+	<title>Mantenimiento Usuario</title>
 </head>
 <body class="with-side-menu">
 
@@ -22,10 +22,10 @@
 				<div class="tbl">
 					<div class="tbl-row">
 						<div class="tbl-cell">
-							<h3>Mantenimiento Usuario</h3>
+							<h3>Mantenimiento usuario</h3>
 							<ol class="breadcrumb breadcrumb-simple">
 								<li><a href="#">Home</a></li>
-								<li class="active">Mantenimiento Usuario</li>
+								<li class="active">Mantenimiento usuario</li>
 							</ol>
 						</div>
 					</div>
