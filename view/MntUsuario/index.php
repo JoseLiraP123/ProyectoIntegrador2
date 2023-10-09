c95ac4376f9770afa1fe2a3d92275445eddccb24
Javier <sanchez.jsebastian99@gmail.com>
2023-10-08 22:44:38 -0500

    Mantenimiento usuario

--- view/MntUsuario/index.php
+++ view/MntUsuario/index.php
@@ -1,69 +1,71 @@
 <?php
-    require_once '../../config/conexion.php';
-    if(isset($_SESSION["usu_id"])){ 
+  require_once("../../config/conexion.php"); 
+  if(isset($_SESSION["usu_id"])){ 
 ?>
-
 <!DOCTYPE html>
 <html>
-    <?php require_once("../MainHeader/head.php"); ?>
-    
-<title>Mantenimiento usuario</title>
+    <?php require_once("../MainHead/head.php");?>
+	<title>AnderCode</>::Mantenimiento Usuario</title>
 </head>
 <body class="with-side-menu">
 
-	<?php require_once("../MainHeader/header.php"); ?>
+    <?php require_once("../MainHeader/header.php");?>
 
-	<div class="mobile-menu-left-overlay"></div>
-        
-	<?php require_once("../MainNav/nav.php"); ?>
+    <div class="mobile-menu-left-overlay"></div>
+    
+    <?php require_once("../MainNav/nav.php");?>
 
+	<!-- Contenido -->
 	<div class="page-content">
 		<div class="container-fluid">
-                    <header class="section-header">
-                        <div class="tbl">
-                            <div class="tbl-row">
-                                <div class="tbl-cell">
-                                    <h3>Mantenimiento usuario</h3>
-                                    <ol class="breadcrumb breadcrumb-simple">
-                                            <li><a href="#">Home</a></li>
-                                            <li class="active">Mantenimiento usuario</li>
-                                    </ol>
-                                </div>
-                            </div>
-                        </div>
-                    </header>
-                    
-                    <div class="box-typical box-typical-padding">
-                    <button type="button"class="btn btn-inline btn-primary" id="btnnuevo">Nuevo registro</button>
-                        <table id="usuario_data" class="table table-bordered table-stripped table-vcenter js-dataTable-full">
-                            <thead>
-                                <tr>
-                                    <th style="width: 5%;">Nombre</th>
-                                    <th style="width: 15%;">Apellido</th>
-                                    <th class="d-none d-sm-table-cell" style="width: 40%">Correo</th>
-                                    <th class="d-none d-sm-table-cell" style="width: 5%">Contraseña</th>
-                                    <th class="d-none d-sm-table-cell" style="width: 10%">Rol</th>
-                                    <th class="text-center" style="width: 5%;"></th>
-                                    <th class="text-center" style="width: 5%;"></th>
-                                </tr>
-                            </thead>
-                        </table>
-                    </div>
-		</div><!--.container-fluid-->
-	</div><!--.page-content-->
+			<header class="section-header">
+				<div class="tbl">
+					<div class="tbl-row">
+						<div class="tbl-cell">
+							<h3>Mantenimiento Usuario</h3>
+							<ol class="breadcrumb breadcrumb-simple">
+								<li><a href="#">Home</a></li>
+								<li class="active">Mantenimiento Usuario</li>
+							</ol>
+						</div>
+					</div>
+				</div>
+			</header>
 
-    <?php require_once("modalmantenimiento.php"); ?>     
+			<div class="box-typical box-typical-padding">
+				<button type="button" id="btnnuevo" class="btn btn-inline btn-primary">Nuevo Registro</button>
+				<table id="usuario_data" class="table table-bordered table-striped table-vcenter js-dataTable-full">
+					<thead>
+						<tr>
+							<th style="width: 10%;">Nombre</th>
+							<th style="width: 10%;">Apellido</th>
+							<th class="d-none d-sm-table-cell" style="width: 40%;">Correo</th>
+							<th class="d-none d-sm-table-cell" style="width: 5%;">Contraseña</th>
+							<th class="d-none d-sm-table-cell" style="width: 5%;">Rol</th>
+							<th class="text-center" style="width: 5%;"></th>
+							<th class="text-center" style="width: 5%;"></th>
+						</tr>
+					</thead>
+					<tbody>
 
+					</tbody>
+				</table>
+			</div>
 
-	<?php require_once("../MainJS/js.php"); ?>
-        <script src="mntusuario.js"></script>
-        
+		</div>
+	</div>
+	<!-- Contenido -->
+
+	<?php require_once("modalmantenimiento.php");?>
+
+	<?php require_once("../MainJs/js.php");?>
+	
+	<script type="text/javascript" src="mntusuario.js"></script>
+
 </body>
 </html>
-<?php  
-    } else {
-        $conexion = new Conectar();
-        $ruta = $conexion->ruta();        
-        header("Location: ".$ruta."index.php");
-    }
+<?php
+  } else {
+    header("Location:".Conectar::ruta()."index.php");
+  }
 ?>
--- view/MntUsuario/mntusuario.js
+++ view/MntUsuario/mntusuario.js
@@ -1,6 +1,35 @@
 var tabla;
 
-function init() {}
+function init(){
+    $("#usuario_form").on("submit",function(e){
+        guardaryeditar(e);	
+    });
+}
+
+function guardaryeditar(e){
+    e.preventDefault();
+	var formData = new FormData($("#usuario_form")[0]);
+    $.ajax({
+        url: "../../controller/usuario.php?op=guardaryeditar",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function(datos){    
+            console.log(datos);
+            $('#usuario_form')[0].reset();
+            $("#modalmantenimiento").modal('hide');
+            $('#usuario_data').DataTable().ajax.reload();
+
+            swal({
+                title: "HelpDesk!",
+                text: "Completado.",
+                type: "success",
+                confirmButtonClass: "btn-success"
+            });
+        }
+    }); 
+}
 
 $(document).ready(function(){
     tabla=$('#usuario_data').dataTable({
@@ -10,93 +39,101 @@ $(document).ready(function(){
         "searching": true,
         lengthChange: false,
         colReorder: true,
-        buttons: [
-            'copyHtml5',
-            'excelHtml5',
-            'csvHtml5',
-            'pdfHtml5',
-        ],
-        "ajax": {
+        buttons: [		          
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5'
+                ],
+        "ajax":{
             url: '../../controller/usuario.php?op=listar',
-            type: "post",
-            dataType: "json",
+            type : "post",
+            dataType : "json",						
             error: function(e){
-                console.log(e.responseText);
+                console.log(e.responseText);	
             }
         },
         "bDestroy": true,
         "responsive": true,
-        "bInfo": true,
+        "bInfo":true,
         "iDisplayLength": 10,
         "autoWidth": false,
         "language": {
-            "sProcesing": "Procesando...",
-            "sLengthMenu": "Mostrar _MENU_ registros",
-            "sZeroRecords": "No se encontraron resultados",
-            "sEmptyTable": "Ningun dato disponible en esta tabla",
-            "sInfo": "Mostrando un total de _TOTAL_ registros",
-            "sInfoEmpty": "Mostrando un total de 0 registros",
-            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-            "sInfoPostFix": "",
-            "sSearch": "Buscar:",
-            "sUrl": "",
-            "sInfoThousands": ",",
+            "sProcessing":     "Procesando...",
+            "sLengthMenu":     "Mostrar _MENU_ registros",
+            "sZeroRecords":    "No se encontraron resultados",
+            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+            "sInfo":           "Mostrando un total de _TOTAL_ registros",
+            "sInfoEmpty":      "Mostrando un total de 0 registros",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":    "",
+            "sSearch":         "Buscar:",
+            "sUrl":            "",
+            "sInfoThousands":  ",",
             "sLoadingRecords": "Cargando...",
             "oPaginate": {
-                "sFirst": "Primero",
-                "sLast": "Último",
-                "sNext": "Siguiente",
+                "sFirst":    "Primero",
+                "sLast":     "Último",
+                "sNext":     "Siguiente",
                 "sPrevious": "Anterior"
             },
-            "oAria":{
-                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
                 "sSortDescending": ": Activar para ordenar la columna de manera descendente"
             }
-        }
-    }).DataTable();
+        }     
+    }).DataTable(); 
 });
 
 function editar(usu_id){
-    $('#mdltitulo').html('Editar registro');
-    $('#modalMantenimiento').modal('show');
+    $('#mdltitulo').html('Editar Registro');
+
+    $.post("../../controller/usuario.php?op=mostrar", {usu_id : usu_id}, function (data) {
+        data = JSON.parse(data);
+        $('#usu_id').val(data.usu_id);
+        $('#usu_nom').val(data.usu_nom);
+        $('#usu_ape').val(data.usu_ape);
+        $('#usu_correo').val(data.usu_correo);
+        $('#usu_pass').val(data.usu_pass);
+        $('#rol_id').val(data.rol_id).trigger('change');
+    }); 
+
+    $('#modalmantenimiento').modal('show');
 }
 
 function eliminar(usu_id){
-    swal(
-        {
-          title: "HelpDesk?",
-          text: "Esta seguro de eliminar el registro?",
-          type: "error",
-          showCancelButton: true,
-          confirmButtonClass: "btn-danger",
-          confirmButtonText: "Si",
-          cancelButtonText: "No",
-          closeOnConfirm: false,
-        },
-        function (isConfirm) {
-          if (isConfirm) {
-            $.post(
-              "../../controller/usuario.php?op=eliminar",
-              { usu_id: usu_id },
-              function (data) {}
-            );
-    
-            $('#usuario_data').DataTable().ajax.reload();
-                
+    swal({
+        title: "HelpDesk",
+        text: "Esta seguro de Eliminar el registro?",
+        type: "error",
+        showCancelButton: true,
+        confirmButtonClass: "btn-danger",
+        confirmButtonText: "Si",
+        cancelButtonText: "No",
+        closeOnConfirm: false
+    },
+    function(isConfirm) {
+        if (isConfirm) {
+            $.post("../../controller/usuario.php?op=eliminar", {usu_id : usu_id}, function (data) {
+
+            }); 
+
+            $('#usuario_data').DataTable().ajax.reload();	
+
             swal({
-              title: "HelpDesk",
-              text: "Registro eliminado.",
-              type: "success",
-              confirmButtonClass: "btn-success",
+                title: "HelpDesk!",
+                text: "Registro Eliminado.",
+                type: "success",
+                confirmButtonClass: "btn-success"
             });
-          }
         }
-      );
+    });
 }
 
-$(document).on("click", "#btnnuevo", function () {
-    $('#mdltitulo').html('Nuevo registro');
-    $('#modalMantenimiento').modal('show');
+$(document).on("click","#btnnuevo", function(){
+    $('#mdltitulo').html('Nuevo Registro');
+    $('#usuario_form')[0].reset();
+    $('#modalmantenimiento').modal('show');
 });
 
 init();
