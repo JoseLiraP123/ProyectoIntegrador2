58551f831f362600bf23f88d71ec565b3c0a50f3
Axel Lucas Ledesma Vargas <134738464+AxelLedesma2510@users.noreply.github.com>
2023-10-09 01:40:05 -0500

    commit fix

--- view/Home/index.php
+++ view/Home/index.php
@@ -1,78 +1,37 @@
 <?php
-  require_once("../../config/conexion.php"); 
-  if(isset($_SESSION["usu_id"])){ 
+    require_once '../../config/conexion.php';
+    if(isset($_SESSION["usu_id"])){ 
 ?>
+
 <!DOCTYPE html>
 <html>
-    <?php require_once("../MainHead/head.php");?>
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
-	<title>AnderCode</>::Home</title>
+    <?php require_once("../MainHeader/head.php"); ?>
+    
+<title>Mesa de Ayuda Soporte Técnico</title>
 </head>
 <body class="with-side-menu">
 
-    <?php require_once("../MainHeader/header.php");?>
+	<?php require_once("../MainHeader/header.php"); ?>
 
-    <div class="mobile-menu-left-overlay"></div>
-    
-    <?php require_once("../MainNav/nav.php");?>
+	<div class="mobile-menu-left-overlay"></div>
+        
+	<?php require_once("../MainNav/nav.php"); ?>
 
-	<!-- Contenido -->
 	<div class="page-content">
 		<div class="container-fluid">
-			<div class="row">
-				<div class="col-xl-12">
-					<div class="row">
-						<div class="col-sm-4">
-	                        <article class="statistic-box green">
-	                            <div>
-	                                <div class="number" id="lbltotal"></div>
-	                                <div class="caption"><div>Total de Tickets</div></div>
-	                            </div>
-	                        </article>
-	                    </div>
-						<div class="col-sm-4">
-	                        <article class="statistic-box yellow">
-	                            <div>
-	                                <div class="number" id="lbltotalabierto"></div>
-	                                <div class="caption"><div>Total de Tickets Abiertos</div></div>
-	                            </div>
-	                        </article>
-	                    </div>
-						<div class="col-sm-4">
-	                        <article class="statistic-box red">
-	                            <div>
-	                                <div class="number" id="lbltotalcerrado"></div>
-	                                <div class="caption"><div>Total de Tickets Cerrados</div></div>
-	                            </div>
-	                        </article>
-	                    </div>
-					</div>
-				</div>
-			</div>
+			Blank page.
+		</div><!--.container-fluid-->
+	</div><!--.page-content-->
 
-			<section class="card">
-				<header class="card-header">
-					Grafico Estadístico
-				</header>
-				<div class="card-block">
-					<div id="divgrafico" style="height: 250px;"></div>
-				</div>
-			</section>
-			
-		</div>
-	</div>
-	<!-- Contenido -->
-
-	<?php require_once("../MainJs/js.php");?>
-
-	<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-	<script type="text/javascript" src="home.js"></script>
-
+	<?php require_once("../MainJS/js.php"); ?>
+        <script src="home.js" type="text/javascript"></script>
+        
 </body>
 </html>
-<?php
-  } else {
-    header("Location:".Conectar::ruta()."index.php");
-  }
+<?php  
+    } else {
+        $conexion = new Conectar();
+        $ruta = $conexion->ruta();        
+        header("Location: ".$ruta."index.php");
+    }
 ?>
